47e9b4adccd7e8c9147a91d96c7a510d2c372c44
Badar Wildanie <badar.serpent@gmail.com>
2021-11-16 14:32:41 +0700

    fix: update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Anggota;
+use App\Models\AnggotaOrganisasiLain;
+use App\Models\AnggotaOrganisasiNu;
+use App\Models\AnggotaPekerjaan;
+use App\Models\AnggotaPendidikan;
+use App\Models\PKP;
+use App\Models\Pondok;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +21,22 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+
+        $pondok = Pondok::factory()->count(5)->create();
+        $pkp = PKP::factory()->count(5)->create();
+        for ($i = 0; $i <= 10; $i++) {
+            $pendidikan = AnggotaPendidikan::factory()
+                ->for($pondok[rand(0, 5 - 1)])
+                ->create();
+            $pekerjaan = AnggotaPekerjaan::factory()->create();
+            $anggota = Anggota::factory()
+                ->has(AnggotaOrganisasiLain::factory()->count(rand(1, 3)), 'organisasiLain')
+                ->has(AnggotaOrganisasiNu::factory()->count(rand(1, 3)), 'organisasiNu')
+                ->for($pendidikan, 'pendidikan')
+                ->for($pekerjaan, 'pekerjaan')
+                ->for($pkp[rand(0, 5 - 1)])
+                ->create();
+        }
     }
 }
