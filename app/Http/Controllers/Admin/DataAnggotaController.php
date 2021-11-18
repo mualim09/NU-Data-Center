2bea0cf090f01d45c7f0695356052640c1d7ec24
Badar Wildanie <badar.serpent@gmail.com>
2021-11-18 16:15:04 +0700

    add: penulisan method database store

--- app/Http/Controllers/Admin/DataAnggotaController.php
+++ app/Http/Controllers/Admin/DataAnggotaController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Anggota;
+use App\Models\AnggotaOrganisasiLain;
+use App\Models\AnggotaOrganisasiNu;
+use App\Models\AnggotaPekerjaan;
+use App\Models\AnggotaPendidikan;
+use App\Models\PKP;
+use App\Models\Pondok;
 use App\Models\Wilayah;
 use Illuminate\Http\Request;
 
@@ -59,9 +65,13 @@ class DataAnggotaController extends Controller
     public function create()
     {
         $data['anggota'] = Anggota::find(1);
-        $data['dataKecamatan'] = Wilayah::orderBy('kecamatan', 'asc')->select('kecamatan')->distinct()->get()->map(function ($item) {
-            return $item->kecamatan;
-        });
+        $data['dataKecamatan'] = Wilayah::orderBy('kecamatan', 'asc')
+            ->select('kecamatan')
+            ->distinct()
+            ->get()
+            ->map(function ($item) {
+                return $item->kecamatan;
+            });
         return view('admin.data_anggota.create', $data);
     }
 
@@ -73,7 +83,86 @@ class DataAnggotaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+
+        $pondok = Pondok::create([
+            'nama' => $request->pondok_nama,
+            'alamat' => $request->pondok_alamat,
+        ]);
+
+        $pendidikan = AnggotaPendidikan::create([
+            'pendidikan_terakhir'=> $request->pendidikan_pendidikan_terakhir,
+            'jurusan'=> $request->pendidikan_jurusan,
+            'pendidikan_pesantren'=> $request->pendidikan_pendidikan_pesantren,
+            'pondok_id' => $pondok->id,
+        ]);
+
+        $pekerjaan = AnggotaPekerjaan::create([
+            'jenis_profesi' => $request->pekerjaan_jenis_profesi,
+            'alamat_kantor' => $request->pekerjaan_alamat_kantor,
+            'penghasilan_perbulan' => $request->pekerjaan_penghasilan_perbulan,
+        ]);
+
+        $pkp = PKP::create([
+            'angkatan_pkp' => $request->pkp_angkatan_pkp,
+            'lokasi_kegiatan' => $request->pkp_lokasi_kegiatan,
+            'waktu_kegiatan' => $request->pkp_waktu_kegiatan,
+        ]);
+
+
+        $pathFotoDiri = 'storage/' . $request->file('foto_diri')->store('anggota/foto_diri');
+        $pathScanKtp = 'storage/' . $request->file('scan_ktp')->store('anggota/scan_ktp');
+        $pathScanKartanu = 'storage/' . $request->file('scan_kartanu')->store('anggota/scan_kartanu');
+
+
+        $anggota = Anggota::create([
+            'no_kartanu' => $request->no_kartanu,
+            'no_ktp' => $request->no_ktp,
+            'nama' => $request->nama,
+            'jenis_kelamin' => $request->jenis_kelamin,
+            'tempat_lahir' => $request->tempat_lahir,
+            'tanggal_lahir' => $request->tanggal_lahir,
+            'no_telepon' => $request->no_telepon,
+            'email' => $request->email,
+            'foto_diri' => $pathFotoDiri,
+            'scan_ktp' => $pathScanKtp,
+            'scan_kartanu' => $pathScanKartanu,
+            'kabupaten' => $request->kabupaten,
+            'kecamatan' => $request->kecamatan,
+            'kelurahan' => $request->kelurahan,
+            'alamat' => $request->alamat,
+            'status_menikah' => $request->status_menikah,
+            'jumlah_anggota_keluarga' => $request->jumlah_anggota_keluarga,
+            'anggota_pendidikan_id' => $pendidikan->id,
+            'anggota_pekerjaan_id' => $pekerjaan->id,
+            'aktifitas_nu' => $request->aktifitas_nu,
+            'jabatan_nu' => $request->jabatan_nu,
+            'asuransi_kesehatan' => $request->asuransi_kesehatan,
+            'pkp_id' => $pkp->id
+        ]);
+
+
+        $organisasiNu = collect(json_decode($request->organisasi_nu));
+        $organisasiNu->each(function ($organisasi) use ($anggota) {
+            AnggotaOrganisasiNu::create([
+                'struktur_organisasi' =>  $organisasi->struktur_organisasi,
+                'jabatan' =>  $organisasi->jabatan,
+                'masa_jabat_awal' =>  ($organisasi->masa_jabat_awal != '' ? $organisasi->masa_jabat_awal : null),
+                'masa_jabat_akhir' =>  ($organisasi->masa_jabat_akhir != '' ? $organisasi->masa_jabat_akhir : null),
+                'anggota_id' => $anggota->id,
+            ]);
+        });
+
+        $organisasiLain = collect(json_decode($request->organisasi_lain));
+        $organisasiLain->each(function ($organisasi) use ($anggota) {
+            AnggotaOrganisasiLain::create([
+                'nama_organisasi' =>  $organisasi->nama_organisasi,
+                'jabatan' =>  $organisasi->jabatan,
+                'masa_jabat_awal' =>  ($organisasi->masa_jabat_awal != '' ? $organisasi->masa_jabat_awal : null),
+                'masa_jabat_akhir' =>  ($organisasi->masa_jabat_akhir != '' ? $organisasi->masa_jabat_akhir : null),
+                'anggota_id' => $anggota->id,
+            ]);
+        });
     }
 
     /**
