f8aab182e7c2ce8d4ee2902275b0a33301ae0aed
Badar Wildanie <badar.serpent@gmail.com>
2021-12-04 14:31:06 +0700

    fix: menambahkan beberapa kolom pada tabel admin

--- database/migrations/2021_11_20_035733_create_admin_table.php
+++ database/migrations/2021_11_20_035733_create_admin_table.php
@@ -18,9 +18,13 @@ class CreateAdminTable extends Migration
             $table->string('username')->unique();
             $table->string('password');
             $table->string('nama_lengkap');
+            $table->string('jenis_kelamin');
             $table->text('avatar');
             $table->date('tanggal_lahir')->nullable();
             $table->text('alamat')->nullable();
+            $table->text('kabupaten')->nullable();
+            $table->text('kecamatan')->nullable();
+            $table->text('kelurahan')->nullable();
             $table->string('nomor_hp', 50)->nullable();
             $table->string('nomor_ktp', 50)->nullable();
             $table->rememberToken();
